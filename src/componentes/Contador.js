b48ece0b9ed30a2b7edbb75077f5f8386e053711
Sergio Andres <sergiussss29@gmail.com>
2024-05-12 23:41:15 +0200

    Primera versión

---
+++ src/componentes/Contador.js
@@ -0,0 +1,12 @@
+import '../stylesheets/contador.css'
+
+
+function Contador ({ numClics }){
+  return (
+    <div className='contador'>
+      {numClics}
+    </div>
+  );
+}
+
+export default Contador;
